09da2575eff261d3752663304615770efa1bd433
vmurali100 <vmurali100@users.noreply.github.com>
2022-12-13 19:30:42 +0530

    First Commit

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,21 @@
 var express = require('express');
 var router = express.Router();
 const mongoose = require("mongoose");
-const db = "mongodb://vmurali:vmurali123@ds029979.mlab.com:29979/allusers"
+const db = "mongodb+srv://vmurali:Pavani!2345@cluster0.plco6qd.mongodb.net/?retryWrites=true&w=majority"
 
 /* GET home page. */
 mongoose.set('useUnifiedTopology',true);
 mongoose.set('useNewUrlParser',true)
 router.get('/', function(req, res, next) {
   mongoose.connect(db,(err)=>{
+    console.log(err)
     if(!err){
       console.log("DB Connected Successfully")
+    }else{
+      console.log(err)
     }
   })
-  res.render('index', { title: 'Express Is checked' });
+  res.render('index', { title: 'Express Is checked..' });
 });
 
 module.exports = router;
